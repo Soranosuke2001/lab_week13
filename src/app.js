1c53aae843f6c8887f09388f470701869cbf666a
Sora <80365991+Liquid-Unit@users.noreply.github.com>
2022-03-30 18:22:19 -0700

    selected words under "originals" will be printed under "copies"

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const all_divs = document.querySelectorAll('.original-container div')
+for (const each_div of all_divs) {
+    each_div.addEventListener('click', () => {
+        const paste_area = document.querySelector('.copy-container')
+        paste_area.insertAdjacentHTML("afterbegin", each_div.outerHTML)
+    });
+}
